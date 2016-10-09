c0b9b3d6b9e944a19ade177f2451bbe51032276b
Jim Cochrane <jim.cochrane@gmail.com>
2016-10-09 02:28:52 -0500

    expiration date

--- src/spectools.rb
+++ src/spectools.rb
@@ -3,5 +3,5 @@ module SpecTools
   TYPE_KEY, TITLE_KEY, DESCRIPTION_KEY, HANDLE_KEY, PRIORITY_KEY, DUE_DATE_KEY,
     GOAL_KEY, MEDIA_KEY, COMMENT_KEY, PARENT_KEY, REMINDER_KEY, START_DATE_KEY =
     'type', 'title', 'description', 'handle', 'priority', 'due_date', 'goal',
-    'media', 'comment', 'parent', 'reminders', 'start_date'
+    'media', 'comment', 'parent', 'reminders', 'start_date', 'expiration_date'
 end
