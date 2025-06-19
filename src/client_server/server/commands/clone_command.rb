63bcddbc4d3fb5a5ccd46abb099cfa573f713ef7
Jim Cochrane <jim.cochrane@gmail.com>
2025-06-19 14:11:05 -0400

    clone: move "clone.force_update" to preceded parent setting

--- src/client_server/server/commands/clone_command.rb
+++ src/client_server/server/commands/clone_command.rb
@@ -12,13 +12,13 @@ class CloneCommand < WorkCommand
       # Guaranteed by 'clone':
       check(:same_parent) { clone.parent_handle == target.parent_handle }
       clone.handle = new_handle
+      # (Call 'force_update' instead of store_target(clone) to ensure that
+      #  clone.prepare_for_db_write is called to prevent Oj nesting error.)
+      clone.force_update
       if ! target.parent_handle.nil? then
         parent = database[target.parent_handle]
         parent.add_child(clone)
       end
-      # (Call 'force_update' instead of store_target(clone) to ensure that
-      #  clone.prepare_for_db_write is called to prevent Oj nesting error.)
-      clone.force_update
     else
       msg = "cloning error: handle #{new_handle} is already in use."
       $log.warn msg
