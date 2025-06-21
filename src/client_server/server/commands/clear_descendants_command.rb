0a9909ef2645ec4dcfaefad317b969a1200b9fef
Jim Cochrane <jim.cochrane@gmail.com>
2025-06-21 18:10:57 -0400

    Implement ClearDescendantsCommand.
    ClearDescendantsCommand.do_execute: Uses the architecture completely -
    not depending on legacy code/classes like STodoManager,
    STodoTargetEditor, etc.
    
    Modified test secript clear-desc-101 to use stodo_client_repl (
    REPL-based CLI client to the (new) stodo server) to ensure that
    ClearDescendantsCommand is tested.

--- src/client_server/server/commands/clear_descendants_command.rb
+++ src/client_server/server/commands/clear_descendants_command.rb
@@ -1,7 +1,24 @@
 class ClearDescendantsCommand < WorkCommand
   include CommandConstants, Contracts::DSL
 
+  CLEAR_D_SEPARATOR = ':'
+
   def do_execute(the_caller)
+    handle_spec = arg1
+    hspec_components = handle_spec.split(/#{CLEAR_D_SEPARATOR}/)
+    handle = hspec_components[0]
+    exceptions = hspec_components[1 .. -1]
+    target = database[handle]
+    if target != nil then
+      orig_descs = target.descendants
+      orig_desc_count = orig_descs.count
+      target.remove_descendants(exceptions)
+      orig_descs = orig_descs - target.descendants
+      orig_descs.each do |d|
+        database.delete(d.handle)
+      end
+      git_commit(target)
+    end
   end
 
 end
